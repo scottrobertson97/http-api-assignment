48893a6ee16ae63362206e76b1d5078e0d035ee7
Scott Robertson <scottrobertson97@gmail.com>
2018-09-16 19:36:13 -0400

    done

--- src/responses.js
+++ src/responses.js
@@ -1,52 +1,104 @@
-const fs = require('fs');
-const index = fs.readFileSync(`${__dirname}/../client/client.html`);
-
-const respond = (request, response, content, type, status) => {
+const respond = (request, response, type, status, content) => {
   response.writeHead(status, { 'Content-Type': type });
   response.write(content);
   response.end();
 };
 
-//function for our /cats page
-//Takes request, response and an array of client requested mime types
-const getCats = (request, response, acceptedTypes) => {
-  //object to send
-  const cat = {
-    name: 'tibbs',
-    age: 4,
-  };
-
-  //if the client's most preferred type (first option listed)
-  //is xml, then respond xml instead
+const success = (request, response, acceptedTypes, params, message = 'This is a successful response.') => {
   if (acceptedTypes[0] === 'text/xml') {
-	//create a valid XML string with name and age tags.
     let responseXML = '<response>';
-    responseXML = `${responseXML} <name>${cat.name}</name>`;
-    responseXML = `${responseXML} <age>${cat.age}</age>`;
-    responseXML = `${responseXML} </response>`;    
+    responseXML = `${responseXML} <message>${message}</message>`;
+    responseXML = `${responseXML} </response>`;
+    return respond(request, response, 'text/xml', 200, responseXML);
+  }
+  // message to send
+  const responseJSON = {
+    message,
+  };
+  // send our json with a success status code
+  return respond(request, response, 'application/json', 200, JSON.stringify(responseJSON));
+};
 
-	//return response passing out string and content type
-    return respond(request, response, responseXML, 'text/xml');
+const error = (request, response, acceptedTypes, params, message, id, statusCode) => {
+  if (acceptedTypes[0] === 'text/xml') {
+    let responseXML = '<response>';
+    responseXML = `${responseXML} <message>${message}</message>`;
+    responseXML = `${responseXML} <id>${id}</id>`;
+    responseXML = `${responseXML} </response>`;
+    return respond(request, response, 'text/xml', statusCode, responseXML);
   }
 
-  //stringify the json object (so it doesn't use references/pointers/etc)
-  //but is instead a flat string object.
-  //Then write it to the response.
-  const catString = JSON.stringify(cat);
-  
-  //return response passing json and content type
-  return respond(request, response, catString, 'application/json');
+  // error message with a description and consistent error id
+  const responseJSON = {
+    message,
+    id,
+  };
+
+  // return our json with a 404 not found error code
+  return respond(request, response, 'application/json', statusCode, JSON.stringify(responseJSON));
 };
 
-//function to handle the index page
-const getIndex = (request, response) => {
-  respond(request, response, index, 'text/html');
+const badRequest = (request, response, acceptedTypes, params) => {
+  if (!params.valid || params.valid !== 'true') {
+    return error(request, response, acceptedTypes, params,
+      'Missing valid query parameter set to true.',
+      'badRequest',
+      400);
+  }
+  return success(request, response, acceptedTypes, params,
+    'This request has the required parameters.');
 };
 
-//exports to set functions to public.
-//In this syntax, you can do getCats:getCats, but if they
-//are the same name, you can short handle to just getCats,
+const unauthorized = (request, response, acceptedTypes, params) => {
+  if (params.loggedIn !== 'yes') {
+    return error(request, response, acceptedTypes, params,
+      'Missing loggedIn query parameter set to yes.',
+      'unauthorized',
+      401);
+  }
+  return success(request, response, acceptedTypes, params,
+    'This request has the required parameters.');
+};
+
+const forbidden = (request, response, acceptedTypes, params) => {
+  error(request, response, acceptedTypes, params,
+    'You do not have access to this content.',
+    'forbidden',
+    403);
+};
+
+const internal = (request, response, acceptedTypes, params) => {
+  error(request, response, acceptedTypes, params,
+    'Internal Server Error. Something went wrong.',
+    'internalError',
+    500);
+};
+
+const notImplemented = (request, response, acceptedTypes, params) => {
+  error(request, response, acceptedTypes, params,
+    'A get request for this page has not been implemented yet. Check again later for updated content.',
+    'notImplemented',
+    501);
+};
+
+// function to show not found error
+const notFound = (request, response, acceptedTypes, params) => {
+  error(request, response, acceptedTypes, params,
+    'The page you are looking for was not found.',
+    'notFound',
+    404);
+};
+
+
+// exports to set functions to public.
+// In this syntax, you can do getCats:getCats, but if they
+// are the same name, you can short handle to just getCats,
 module.exports = {
-  getCats,
-  getIndex,
+  success,
+  badRequest,
+  unauthorized,
+  forbidden,
+  internal,
+  notImplemented,
+  notFound,
 };
--- src/server.js
+++ src/server.js
@@ -1,44 +1,55 @@
 const http = require('http'); // pull in the http server module
 const url = require('url'); // pull in the url module
-//pull in our response handler file
-const responseHandler = require('./responses.js'); 
 
-//set the port. process.env.PORT and NODE_PORT are for servers like heroku
+const query = require('querystring');
+
+// pull in our response handler file
+const responseHandler = require('./responses.js');
+const htmlHandler = require('./htmlResponses.js');
+
+// set the port. process.env.PORT and NODE_PORT are for servers like heroku
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-//key:value object to look up URL routes to specific functions
+// key:value object to look up URL routes to specific functions
 const urlStruct = {
-  '/': responseHandler.getIndex,
-  '/success': ,
-  '/badRequest': ,
-  '/unauthorized': ,
-  '/forbidden': ,
-  '/internal': ,
-  '/notImplemented': ,
-  '/notFound': ,
-  index: responseHandler.getIndex,
+  '/': htmlHandler.getIndex,
+  '/style.css': htmlHandler.getCss,
+  '/favicon.ico': htmlHandler.getFavicon,
+  '/success': responseHandler.success,
+  '/badRequest': responseHandler.badRequest,
+  '/unauthorized': responseHandler.unauthorized,
+  '/forbidden': responseHandler.forbidden,
+  '/internal': responseHandler.internal,
+  '/notImplemented': responseHandler.notImplemented,
+  notFound: responseHandler.notFound,
 };
 
-//handle HTTP requests. In node the HTTP server will automatically
-//send this function request and pre-filled response objects.
+// handle HTTP requests. In node the HTTP server will automatically
+// send this function request and pre-filled response objects.
 const onRequest = (request, response) => {
-  //parse the url using the url module
-  //This will let us grab any section of the URL by name
+  // parse the url using the url module
+  // This will let us grab any section of the URL by name
   const parsedUrl = url.parse(request.url);
 
-  //grab the 'accept' headers (comma delimited) and split them into an array
+  // grab the query parameters (?key=value&key2=value2&etc=etc)
+  // and parse them into a reusable object by field name
+  const params = query.parse(parsedUrl.query);
+
+  // grab the 'accept' headers (comma delimited) and split them into an array
   const acceptedTypes = request.headers.accept.split(',');
 
-  //check if the path name (the /name part of the url) matches 
-  //any in our url object. If so call that function. If not, default to index.
+  console.log(`Getting: ${parsedUrl.pathname}`);
+
+  // check if the path name (the /name part of the url) matches
+  // any in our url object. If so call that function. If not, default to notFound.
   if (urlStruct[parsedUrl.pathname]) {
-    urlStruct[parsedUrl.pathname](request, response, acceptedTypes);
+    urlStruct[parsedUrl.pathname](request, response, acceptedTypes, params);
   } else {
-    urlStruct.index(request, response, acceptedTypes);
+    urlStruct.notFound(request, response, acceptedTypes, params);
   }
 };
 
-//start HTTP server 
+// start HTTP server
 http.createServer(onRequest).listen(port);
 
 console.log(`Listening on 127.0.0.1: ${port}`);
